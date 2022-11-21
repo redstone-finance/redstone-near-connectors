5ca2df294298d09fe7c74780d86251b6d85d1cf5
hatskier <suvorovalex1997@gmail.com>
2022-11-21 21:09:49 +0100

    test: added more tests to the js connector

--- js/test/integration.test.ts
+++ js/test/integration.test.ts
@@ -1,10 +1,6 @@
 import { getOracleValue } from "../src";
-import {
-  asciiToBytes,
-  bytesToAscii,
-  fromHexString,
-  toHexString,
-} from "../src/bytes";
+import { asciiToBytes, fromHexString, toHexString } from "../src/bytes";
+import { requestRedstonePayload } from "redstone-sdk";
 
 const REDSTONE_PAYLOAD_HEX_STR =
   "4254430000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003d1e382100045544800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002e90edd00001812f2590c000000020000002c1296a449f5d353c8b04eb389f33a583ee79449cca6e366900042f19f2521e722a410929223231905839c00865af68738f1a202478d87dc33675ea5824f343901b4254430000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003d1e382100045544800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002e90edd00001812f2590c000000020000002dbbf8a0e6b1c9a56a4a0ef7089ef2a3f74fbd21fbd5c7c8192b70084004b4f6d37427507c4fff835f74fd4d000b6830ed296e207f49831b96f90a4f4e60820ee1c0002312e312e3223746573742d646174612d66656564000014000002ed57011e0000";
@@ -15,6 +11,8 @@ const SIGNER_1_PUB_KEY_HEX =
 const SIGNER_2_PUB_KEY_HEX =
   "4f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa385b6b1b8ead809ca67454d9683fcf2ba03456d6fe2c4abe2b07f0fbdbb2f1c1";
 const TEST_TIMESTAMP_MILLISECONDS = 1654353400000;
+const EUR_BYTES_32_HEX =
+  "4555520000000000000000000000000000000000000000000000000000000000";
 
 // ========================== RedStone payload structure (hex) ==========================
 //   "4254430000000000000000000000000000000000000000000000000000000000" + // bytes32("BTC")
@@ -62,8 +60,8 @@ const mockEcrecover = (
   }
 };
 
-describe("Integration", () => {
-  test("Should properly get oracle value", async () => {
+describe("Integration tests", () => {
+  test("Should properly get oracle value with correct test payload", async () => {
     const oracleValue = getOracleValue({
       authorisedSigners: [SIGNER_1_PUB_KEY_HEX, SIGNER_2_PUB_KEY_HEX],
       currentTimestampMilliseconds: TEST_TIMESTAMP_MILLISECONDS,
@@ -75,5 +73,67 @@ describe("Integration", () => {
     });
 
     expect(oracleValue).toBe(BigInt(4200000000000));
+  });
+
+  test("Should properly get oracle value with real redstone payload", async () => {
+    const redstoneDataGateways = [
+      "https://cache-service-direct-1.b.redstone.finance",
+      "https://d33trozg86ya9x.cloudfront.net",
+    ];
+
+    const redstonePayloadHex = await requestRedstonePayload(
+      {
+        dataServiceId: "redstone-main-demo",
+        uniqueSignersCount: 1,
+        dataFeeds: ["BTC"],
+      },
+      redstoneDataGateways
+    );
+
+    const realBtcPriceFromOracle = getOracleValue({
+      authorisedSigners: [SIGNER_2_PUB_KEY_HEX],
+      currentTimestampMilliseconds: TEST_TIMESTAMP_MILLISECONDS,
+      dataFeedId: BTC_BYTES_32_HEX,
+      redstonePayload: fromHexString(redstonePayloadHex),
+      uniqueSignersThreshold: 1,
+      ecrecover: mockEcrecover,
+      keccak256: mockKeccak256,
+    });
+
+    console.log({ realBtcPriceFromOracle });
+    expect(realBtcPriceFromOracle).toBeGreaterThan(0);
+  });
+
+  test("Should fail for corrupted payload", async () => {
+    expect(() =>
+      getOracleValue({
+        authorisedSigners: [SIGNER_1_PUB_KEY_HEX, SIGNER_2_PUB_KEY_HEX],
+        currentTimestampMilliseconds: TEST_TIMESTAMP_MILLISECONDS,
+        dataFeedId: BTC_BYTES_32_HEX,
+        redstonePayload: fromHexString(
+          REDSTONE_PAYLOAD_HEX_STR.replace(
+            "000002ed57011e0000",
+            "000002ed57011b0000"
+          )
+        ),
+        uniqueSignersThreshold: 2,
+        ecrecover: mockEcrecover,
+        keccak256: mockKeccak256,
+      })
+    ).toThrow("Assertion failed with: Invalid redstone marker");
+  });
+
+  test("Should fail if requesting missing data feed id", () => {
+    expect(() =>
+      getOracleValue({
+        authorisedSigners: [SIGNER_1_PUB_KEY_HEX, SIGNER_2_PUB_KEY_HEX],
+        currentTimestampMilliseconds: TEST_TIMESTAMP_MILLISECONDS,
+        dataFeedId: EUR_BYTES_32_HEX,
+        redstonePayload: fromHexString(REDSTONE_PAYLOAD_HEX_STR),
+        uniqueSignersThreshold: 2,
+        ecrecover: mockEcrecover,
+        keccak256: mockKeccak256,
+      })
+    ).toThrow("Insufficient number of unique signers: 0");
   });
 });
